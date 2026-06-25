9b2d1773925dd31017cd3c3676d5c0aaf85d3864
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-06-25 10:39:52 +0900

    로그인 페이지 화면 작업 완료

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,24 +3,25 @@ import { Button, Form } from "react-bootstrap";
 
 function LoginPage() {
     return (
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-            </Form.Group>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+            <Form style={{ width: "30rem", marginTop: "3rem" }}>
+                <p style={{ fontSize: "2rem", fontWeight: "bold" }}>로그인</p>
+                <p style={{ fontSize: "1.5rem", marginBottom: "3rem" }}>국내 유일 중고・경매 통합 사이트</p>
+                <Form.Group className="mb-3 login-page-form-group" controlId="formBasicEmail">
+                    <Form.Label>이메일</Form.Label>
+                    <Form.Control type="email" placeholder="이메일을 입력해주세요" />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+                <Form.Group className="mb-3 login-page-form-group" controlId="formBasicPassword">
+                    <Form.Label>비밀번호</Form.Label>
+                    <Form.Control type="password" placeholder="비밀번호" />
+                </Form.Group>
+                <Button variant="primary" type="submit" size="lg" style={{ margin: "1rem" }}>
+                    로그인
+                </Button>
+                <p style={{ textDecoration: "underLine", cursor: "pointer" }}>아직 가입하시지 않으셨나요?</p>
+            </Form>
+        </div>
     );
 }
 
