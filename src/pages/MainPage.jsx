06d222a128d54a65d3abf1c67ae3a251cdc05f5f
gomsik811 <gomsik811@gmail.com>
2026-06-25 12:42:43 +0900

    매인페이지 기초작업 완료

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -22,25 +22,29 @@ function MainPage() {
 
         <Container>
 
-            <Row style={{border:'6px solid green', borderRadius: '15px'}}>
-                <Col>
-                    <span className="MainPage-Font-Main" style={{color:'red'}}>중고</span>
-                    <div style={{display: 'flex',flexDirection:'column',alignItems: 'center'}}>
+            <Row style={{ columnGap: '20px', position: 'relative' }}>
+                <Col style={{ flex: '1', border: '2px solid gray', borderRadius: '15px', borderRight: '2px solid gray', paddingRight: '20px' }}>
+                <div className="MainPage-Title"><img src="/images/shopping_basket.png"alt="중고 거래"/>
+                    <span className="MainPage-Font-Main" style={{ color: 'orange' }}>중고 거래</span>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                         {
                             dataset.map((data, index) => {
                                 if (index > 5) {
                                     return;
                                 }
                                 return (
-                                    <Card style={{ width: '80%',height:'15rem' ,display:'flex', flexDirection:'row',
-                                        marginBottom:'18px', padding: '12px', border:'none', borderRadius:'18px',backgroundColor:'orange'}} key={index}>
-                                        <Card.Img variant="string" src={'images/' + data.img} className="MainPage-picture-size"/>
+                                    <Card style={{
+                                        width: '80%', height: '15rem', display: 'flex', flexDirection: 'row',
+                                        marginBottom: '18px', padding: '12px', border: 'none', borderRadius: '18px', border:'2px solid orange'
+                                    }} key={index}>
+                                        <Card.Img variant="string" src={'images/' + data.img} className="MainPage-picture-size" />
                                         <Card.Body className="MainPage-card-body">
-                                            <Card.Title style={{fontSize:'1.5rem'}}>{data.제목}</Card.Title>
+                                            <Card.Title style={{ fontSize: '1.5rem' }}>{data.제목}</Card.Title>
                                             <Card.Text className="MainPage-Font-Text-Size">
                                                 카테고리:{data.카테고리}
                                             </Card.Text>
-                                            <Card.Text className="MainPage-Font-Text-Size">
+                                            <Card.Text className="MainPage-Font-Text-Size" style={{color:'orange'}}>
                                                 가격:{data.가격}
                                             </Card.Text>
                                             <Card.Text className="MainPage-Font-Text-Size">
@@ -56,10 +60,41 @@ function MainPage() {
 
 
                 </Col>
-                <Col>
-                    <span className="MainPage-Font-Main" style={{color:'blue'}}>경매</span>
-                    <div>
+                
+                <Col style={{ flex: '1', border: '2px solid gray', borderRadius: '15px', paddingLeft: '20px' }}>
+                    <div className="MainPage-Title"><img src="/images/auction.png"alt="중고"/>
+                    <span className="MainPage-Font-Main" style={{ color: 'blue' }}>경매</span>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        {
+                            dataset.map((data, index) => {
+                                if (index > 5) {
+                                    return;
+                                }
+                                return (
+                                    <Card style={{
+                                        width: '80%', height: '15rem', display: 'flex', flexDirection: 'row',
+                                        marginBottom: '18px', padding: '12px', border: 'none', borderRadius: '18px', border:'2px solid skyblue'
+                                    }} key={index}>
+                                        <Card.Img variant="string" src={'images/' + data.img} className="MainPage-picture-size" />
+                                        <Card.Body className="MainPage-card-body">
+                                            <Card.Title style={{ fontSize: '1.5rem' }}>{data.제목}</Card.Title>
+                                            <Card.Text className="MainPage-Font-Text-Size">
+                                                카테고리:{data.카테고리}
+                                            </Card.Text>
+                                            <Card.Text className="MainPage-Font-Text-Size" style={{color:'blue'}}>
+                                                가격:{data.가격}
+                                            </Card.Text>
+                                            <Card.Text className="MainPage-Font-Text-Size">
+                                                조회수:{data.조회수}
+                                            </Card.Text>
+                                            <Card.Text>남은시간: 3일 01:11:23s</Card.Text>
+                                        </Card.Body>
+                                    </Card>
 
+                                );
+                            })
+                        }
                     </div>
 
 
