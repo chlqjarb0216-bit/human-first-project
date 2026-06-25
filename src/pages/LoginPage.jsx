586116004dc5c60b0dd2a9e7de3b6706ab5062b7
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-06-25 10:59:33 +0900

    페이지 양식 수정 input 가로폭 조정

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,10 +3,10 @@ import { Button, Form } from "react-bootstrap";
 
 function LoginPage() {
     return (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-            <Form style={{ width: "30rem", marginTop: "3rem" }}>
-                <p style={{ fontSize: "2rem", fontWeight: "bold" }}>로그인</p>
-                <p style={{ fontSize: "1.5rem", marginBottom: "3rem" }}>국내 유일 중고・경매 통합 사이트</p>
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+            <p style={{ fontSize: "2rem", fontWeight: "bold", marginTop: "3rem" }}>로그인</p>
+            <p style={{ fontSize: "1.5rem", marginBottom: "3rem" }}>국내 유일 중고・경매 통합 사이트</p>
+            <Form style={{ width: "20rem" }}>
                 <Form.Group className="mb-3 login-page-form-group" controlId="formBasicEmail">
                     <Form.Label>이메일</Form.Label>
                     <Form.Control type="email" placeholder="이메일을 입력해주세요" />
@@ -14,13 +14,13 @@ function LoginPage() {
 
                 <Form.Group className="mb-3 login-page-form-group" controlId="formBasicPassword">
                     <Form.Label>비밀번호</Form.Label>
-                    <Form.Control type="password" placeholder="비밀번호" />
+                    <Form.Control type="password" placeholder="비밀번호를 입력해주세요" />
                 </Form.Group>
                 <Button variant="primary" type="submit" size="lg" style={{ margin: "1rem" }}>
                     로그인
                 </Button>
-                <p style={{ textDecoration: "underLine", cursor: "pointer" }}>아직 가입하시지 않으셨나요?</p>
             </Form>
+            <p style={{ textDecoration: "underLine", cursor: "pointer" }}>아직 가입하시지 않으셨나요?</p>
         </div>
     );
 }
