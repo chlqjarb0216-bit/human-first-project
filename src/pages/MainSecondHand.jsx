fe5082a7c46694ca11f72dd57ae96a09cb6e522a
gomsik811 <gomsik811@gmail.com>
2026-06-26 17:21:58 +0900

    중고메인페이지 틀 완료

--- src/pages/MainSecondHand.jsx
+++ src/pages/MainSecondHand.jsx
@@ -1,25 +1,70 @@
 import '../csss/MainSecondHand.css';
-import { Container, Row, Col, Card, CardImg } from "react-bootstrap";
-import { useState } from 'react';
+import { Container, Row, Col, Card, CardImg, CardGroup } from "react-bootstrap";
+import { useState, useEffect } from 'react';
 import dataset from '../datas/dataset.json';
 
 function MainSecondHand() {
 
 
-let [datas,setDatas] = useState(dataset);
+
+    let [datas, setDatas] = useState(dataset);
+    let babo = [...dataset];
 
 
+    datas.sort((a, b) => {
+        return (
+            Number(b.조회수) - Number(a.조회수)
+        )
+
+    })
+
+    let babobabo = babo.filter((item)=>{
+        return(
+            item.카테고리 === '굿즈'
+        );
+    })
+
+    let gg =  dataset.filter((item)=>{
+        return(
+            item.카테고리 === '장난감'
+        )
+    })
+
+    let st = 'red';
+    let hot = {'color':st}
 
 
+    let [gang,setgang]  = useState({color:'red'});
+
+    function changeGang(){
+        console.log(gang);
+
+        console.log('gang color: '+gang.color) 
+        if( gang.color == 'blue'){
+            console.log('blue')
+            setgang({color:'red'})
+        }else {
+            console.log('red')
+            setgang({color:'blue'})               
+        }  
+    }
+
+    useEffect(()=>{
+        // const intv= setTimeout(changeGang(), 1000)
+        const tm = setTimeout(changeGang, 1000)
+        return ()=>{
+            clearTimeout(tm);
+        }
+    }, [gang])
 
 
     return (
-        
-        <Container style={{width:'100%',margin:'0',padding:'0'}}>           
-            <div style={{display:'flex',justifyContent:'space-between'}}>
-                <div style={{width:'23%',border:'1px solid black', position: 'sticky'}}>
+
+        <Container style={{ width: '100%', margin: '0', padding: '0' }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start'}}>
+                <div style={{ width: '23%', border: '1px solid black', position: 'sticky', top:'80px', borderRadius:'10px' }}>
                     {/* 카테고리 영역 */}
-                    <p style={{fontWeight:'bold',fontSize:'30px'}}>중고카테고리</p>
+                    <p style={{ fontWeight: 'bold', fontSize: '30px' }}>중고카테고리</p>
                     <p>생활/가전</p>
                     <p>낚시용품</p>
                     <p>it기기</p>
@@ -31,14 +76,125 @@ let [datas,setDatas] = useState(dataset);
                     <p>굿즈</p>
                 </div>
 
-
-
-
-                <div style={{width:'73%',border:'1px solid orange'}}>
+                <div style={{ width: '73%', height:'fit-content' }}>
+                    <div className='SecondHand-section'>
                     {/* 카드 목록 영역 */}
+                    <p className='SecondHand-fontstyle'>🔥인기상품<span style={gang}>HOT</span></p>
+                    <CardGroup>
+                        {
+                            datas.map((data, index) => {
+                                if (index > 2) {
+                                    return;
+                                }
+                                return(
+                                <Card>
+                                    <Card.Img variant="string" src={'images/' + data.img} className='MainSecondHand-Photo-Size' />
+                                    <Card.Body>
+                                        <Card.Title>{data.제목}</Card.Title>
+                                        <Card.Text>
+                                            {/* 첫번째 인기품목 가격 등 들어갈곧 */}
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <Card.Text style={{margin:'0'}}>
+                                            <small className="text-muted">태그:{data.태그}</small>
+                                        </Card.Text>
+                                        <Card.Text>
+                                            <small className="text-muted">카테고리:{data.카테고리}</small>
+                                        </Card.Text>
+                                        
+                                        
+                                    </Card.Footer>
+                                    
+                                </Card>
+                                );
+                            })
+                        }
+                    </CardGroup>
+                    </div>
 
+                
+
+
+
+                <div className='SecondHand-section'>
+                    {/* 카드 목록 영역 */}
+                    <p className='SecondHand-fontstyle'>굿즈</p>
+                    <CardGroup>
+                        {
+                            babobabo.map((data, index) => {
+                                if (index > 2) {
+                                    return;
+                                }
+                                return(
+                                <Card>
+                                    <Card.Img variant="string" src={'images/' + data.img} className='MainSecondHand-Photo-Size' />
+                                    <Card.Body>
+                                        <Card.Title>{data.제목}</Card.Title>
+                                        <Card.Text>
+                                            {/* 첫번째 인기품목 가격 등 들어갈곧 */}
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <Card.Text style={{margin:'0'}}>
+                                            <small className="text-muted">태그:{data.태그}</small>
+                                        </Card.Text>
+                                        <Card.Text>
+                                            <small className="text-muted">카테고리:{data.카테고리}</small>
+                                        </Card.Text>
+                                        
+                                        
+                                    </Card.Footer>
+                                    
+                                </Card>
+                                );
+                            })
+                        }
+                    </CardGroup>
                     
+
                 </div>
+
+                        <div className='SecondHand-section'>
+                    {/* 카드 목록 영역 */}
+                    <p className='SecondHand-fontstyle'>장난감</p>
+                    <CardGroup>
+                        {
+                            gg.map((data, index) => {
+                                if (index > 2) {
+                                    return;
+                                }
+                                return(
+                                <Card>
+                                    <Card.Img variant="string" src={'images/' + data.img} className='MainSecondHand-Photo-Size' />
+                                    <Card.Body>
+                                        <Card.Title>{data.제목}</Card.Title>
+                                        <Card.Text>
+                                            {/* 첫번째 인기품목 가격 등 들어갈곧 */}
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <Card.Text style={{margin:'0'}}>
+                                            <small className="text-muted">태그:{data.태그}</small>
+                                        </Card.Text>
+                                        <Card.Text>
+                                            <small className="text-muted">카테고리:{data.카테고리}</small>
+                                        </Card.Text>
+                                        
+                                        
+                                    </Card.Footer>
+                                    
+                                </Card>
+                                );
+                            })
+                        }
+                    </CardGroup>
+                    
+
+                </div>
+                
+                </div>
+                
 
 
 
